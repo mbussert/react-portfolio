99db070c5918ec697b6d190c84365eb2b5405891
Mike Bussert <mbussert@gmail.com>
2021-06-14 17:11:47 -0400

    Updates

---
+++ precache-manifest.2471cd146d4d5a95db288b9d1361667d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9adc08858967738536250d5a4c0941",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1faa8f7486d020dc5acc",
+    "url": "/react-portfolio/static/css/main.e9b0540d.chunk.css"
+  },
+  {
+    "revision": "1e26111b96b213d559f5",
+    "url": "/react-portfolio/static/js/2.0e38dc01.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/react-portfolio/static/js/2.0e38dc01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1faa8f7486d020dc5acc",
+    "url": "/react-portfolio/static/js/main.263f3bbc.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "d42cdb53e389504f311e0ab56fcf3655",
+    "url": "/react-portfolio/static/media/bootstrap-logo.d42cdb53.svg"
+  },
+  {
+    "revision": "37ebf316f713d0a1f6a2e44c50c204ea",
+    "url": "/react-portfolio/static/media/budgettracker.37ebf316.png"
+  },
+  {
+    "revision": "79afcf47cfc886718c2b3b465c1ae544",
+    "url": "/react-portfolio/static/media/css-logo.79afcf47.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/react-portfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "bfbded945a974c81c2d32b34d0d3a6e6",
+    "url": "/react-portfolio/static/media/empdir.bfbded94.png"
+  },
+  {
+    "revision": "9de32921fe1e3899c721d066d10da3ef",
+    "url": "/react-portfolio/static/media/expressjs-logo.9de32921.svg"
+  },
+  {
+    "revision": "c6dfaaa96f832512245c888c1e8c995e",
+    "url": "/react-portfolio/static/media/fitnesstracker.c6dfaaa9.png"
+  },
+  {
+    "revision": "c50581265217595db341f0dad6192d1f",
+    "url": "/react-portfolio/static/media/giftez.c5058126.png"
+  },
+  {
+    "revision": "aff330687767c3ca0d008f48ea356443",
+    "url": "/react-portfolio/static/media/github-logo.aff33068.svg"
+  },
+  {
+    "revision": "9f2e1637c83e37e8f555044e12324734",
+    "url": "/react-portfolio/static/media/html5-logo.9f2e1637.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/react-portfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/react-portfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "592087c18afd4a594be9b933c6d83414",
+    "url": "/react-portfolio/static/media/insure.592087c1.png"
+  },
+  {
+    "revision": "9c8b14503c2fa7cb9f9188dcaf715416",
+    "url": "/react-portfolio/static/media/js-logo.9c8b1450.svg"
+  },
+  {
+    "revision": "554c7d4bfd63a3da33773978fd291c06",
+    "url": "/react-portfolio/static/media/mongodb-logo.554c7d4b.svg"
+  },
+  {
+    "revision": "78d3a4b1edc15d38509c69ecfe6beb84",
+    "url": "/react-portfolio/static/media/mysql-logo.78d3a4b1.svg"
+  },
+  {
+    "revision": "5c376133d6403088a87a04fcc9685674",
+    "url": "/react-portfolio/static/media/nodejs-logo.5c376133.svg"
+  },
+  {
+    "revision": "7038477259b6f1696308f867ce8c0a3a",
+    "url": "/react-portfolio/static/media/reactjs-logo.70384772.svg"
+  },
+  {
+    "revision": "705dcdcf4ec036808b8690660732ddfb",
+    "url": "/react-portfolio/static/media/sass-logo.705dcdcf.svg"
+  },
+  {
+    "revision": "760a3ee424f40cd7b8da50ac4818592f",
+    "url": "/react-portfolio/static/media/tailwind-logo.760a3ee4.svg"
+  },
+  {
+    "revision": "a89839c6c664af6c1df6ee5aef10353f",
+    "url": "/react-portfolio/static/media/techblog.a89839c6.png"
+  }
+]);
